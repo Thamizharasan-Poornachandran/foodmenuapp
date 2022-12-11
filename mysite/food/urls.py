ef95cd3848f0a7a262e08758c7ae5b73ab0caf60
GitLearningNow <git.learning.now@gmail.com>
2022-12-11 20:50:27 +0530

    Second commit

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 app_name='food'
 urlpatterns = [
     #/food/
-    path('', views.index,name='index'),
+    #path('', views.index,name='index'),
+    path('', views.IndexClassView.as_view(),name='index'),
+    #detail view
+    # path('<int:item_id>/',views.detail,name='detail'),
+    path('<int:pk>/', login_required(views.FoodDetail.as_view()), name='detail'),
     #update view index
     path('update_view/', views.update_index,name='update_index'),
     #/food/1
-    path('<int:item_id>/',views.detail,name='detail'),
-    #
     path('item/', views.item,name='item'),
-    path('add/',views.create_item,name='create_item'),
+    #Function and Class urls
+    #path('add/',views.create_item,name='create_item'),
+    path('add/',login_required(views.CreateItem.as_view()),name='create_item'),
+
     path('update/<int:id>/',views.update_item,name='update_item'),
     path('delete/<int:id>/',views.delete_item,name='delete_item'),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -4,17 +4,40 @@ from django.http import HttpResponse
 from food.models import Item
 from food.forms import ItemForm
 from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 
 # Create your views here.
+'''#Index function based views
 def index(request):
     item_list = Item.objects.all()
     #template = loader.get_template('food/index.html')
-    context = {
-        'item_list':item_list,
-    }
+    context = {'item_list':item_list,}
     #return HttpResponse(template.render(context,request))
     return render(request,'food/index.html',context)
+    '''
+
+#Index class based views -List view -- We listed that food items
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
+#Detail
+'''
+@login_required
+def detail(request,item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {'item': item,}
+    return render(request,'food/detail.html',context)
+'''
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+
 
 #Update index
 @login_required
@@ -28,14 +51,7 @@ def update_index(request):
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
 
-@login_required
-def detail(request,item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item': item,
-    }
-    return render(request,'food/detail.html',context)
-
+'''
 @login_required
 def create_item(request):
     form = ItemForm(request.POST or None)
@@ -43,6 +59,16 @@ def create_item(request):
         form.save()
         return redirect('food:index')
     return render(request,'food/item-form.html',{'form':form})
+'''
+#class based view fot create
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name','item_desc','item_price','item_image']
+    template_name = 'food/item-form.html'
+    def form_valid(self, form):
+        form.instance.user_name=self.request.user
+        return super().form_valid(form)
+
 
 @login_required
 def update_item(request,id):
